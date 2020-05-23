ead0949deeb345b3e8af3ce69ab6b9f83553b8da
mikeku <mikekuforgit@gmail.com>
2020-05-23 15:53:07 +0800

    建立movies應用程式首頁

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def index(request):
+    movies = Movie.objects.all()
+
+    context = {
+        'movies': movies
+    }
+
+    return render(request, 'movies/index.html', context)

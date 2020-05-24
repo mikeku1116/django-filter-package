453d0c4189757054e1e095000c1b1edd2f8c61b1
mikeku <mikekuforgit@gmail.com>
2020-05-24 13:22:07 +0800

    美化movies應用程式的首頁查詢表單

--- movies/filters.py
+++ movies/filters.py
@@ -1,10 +1,19 @@
-import django_filters
+from django import forms
 from .models import Movie
+import django_filters
 
 
 class MovieFilter(django_filters.FilterSet):
 
-    title = django_filters.CharFilter(lookup_expr='icontains')
+    title = django_filters.CharFilter(
+        lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    genre = django_filters.CharFilter(
+        widget=forms.Select(choices=(('', '請選擇'),) + Movie.GENRE_CHOICES, attrs={'class': 'form-control'}))
+
+    release_year = django_filters.CharFilter(
+        widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Movie
